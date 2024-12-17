79cae998fd3bca4157186de2d4f2e2d946a33bf1
Aleksandr Chasnyk <alexandr.chasnyk@yandex.com>
2024-12-17 11:53:16 +0200

    added datasheet

--- hc8_co2_uart/HC8_CO2_uart.h
+++ hc8_co2_uart/HC8_CO2_uart.h
@@ -12,10 +12,16 @@ class HC8CO2UART : public PollingComponent, public uart::UARTDevice {
   HC8CO2UART() : PollingComponent(60000) {}  // Poll every 60 seconds
 
   void set_co2_sensor(sensor::Sensor *co2_sensor) { co2_sensor_ = co2_sensor; }
-
+//  void loop() override;	
   void setup() override;
   void update() override;
   void dump_config() override;
 
  protected:
-  sensor::
+  sensor::Sensor *co2_sensor_{nullptr};
+
+  bool validate_checksum_(const uint8_t *data);
+};
+
+}  // namespace hc8_co2_uart
+}  // namespace esphome
